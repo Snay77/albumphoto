6f9227c832b8852f42ba9f321f7d8595bad9f7ef
Lou-Anne <saejinmx@gmail.com>
2023-11-20 22:36:22 +0100

    Début du formulaire

--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -4,7 +4,8 @@
     
     <form action="{{route(nvalbum)}}" method="POST">
         @csrf
-        
+        <label for="titre">Titre de l'album :</label>
+        <input type="text" name="titre" id="titre">
     </form>
 
 @endsection
