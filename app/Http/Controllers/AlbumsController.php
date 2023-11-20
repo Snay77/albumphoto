3e6b67a4f6cece448fb47cb4bcc3813dee5c5841
Snay77 <77ethan.barlet@gmail.com>
2023-11-20 09:59:32 +0100

    changement avec les nouveaux controller

---
+++ app/Http/Controllers/AlbumsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Album;
+use Illuminate\Http\Request;
+
+class AlbumsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $albums = Album::All();
+        return view('albums', compact("albums",));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Album $album)
+    {
+        return view('album', compact("album",));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Album $album)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Album $album)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Album $album)
+    {
+        //
+    }
+}
