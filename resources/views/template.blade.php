e4fb67b073cf5d92455e1c32b067a5d02126af63
Lou-Anne <saejinmx@gmail.com>
2023-11-20 10:10:22 +0100

    Ajout logo mignon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,6 +5,7 @@
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AlbumPhoto</title>
+    <link rel="icon" href="{{asset('img/logo.png')}}">
 </head>
 <body>
     <header>
