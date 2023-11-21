916ed1c674e653651d75f57964944417ddc02a9e
Lou-Anne <saejinmx@gmail.com>
2023-11-21 16:18:22 +0100

    Formulaire upload fichiers

--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -6,6 +6,8 @@
         @csrf
         <label for="titre">Titre de l'album :</label>
         <input type="text" name="titre" id="titre">
+        <label for="photo">Séléctionnez des photos</label>
+        <input type="file" name="photo[]" multiple id="photo">
     </form>
 
 @endsection
