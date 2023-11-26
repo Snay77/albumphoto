36280bb0994e40301454dd3a50b12b623951fe53
Saejin <saejinmx@gmail.com>
2023-11-27 00:30:12 +0100

    Avancée home page

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 
-    @auth
+    {{-- @auth
         <h1>Bonjour {{Auth::user()->name}}</h1>
-    @endauth
+    @endauth --}}
 
     <section id="bandeau1">
         <div>
-            <h1>Des albums photos pour les plus beaux toutous !</h1>
-            <button>Voir tous les albums</button>
+            <h1>Des albums photos pour les plus beaux <br/> toutous !</h1>
+            <button onclick="window.location.href = '{{route('albums.index')}}';">Voir tous les albums</button>
         </div>
         <img src="{{asset('img/accueil.jpg')}}" alt="dogo">
     </section>
 
     <section id="bandeau2">
-        <b>Admirez et partagez les plus belles photos de vos petits compagnons.</b>
+        <b>Admirez et partagez les plus belles photos <br/> de vos petits compagnons.</b>
     </section>
 
     <section id="bandeau3">
@@ -23,17 +23,17 @@
         <div id="selection">
             <div class="select1 select">
                 <img src="https://i.pinimg.com/564x/19/83/15/198315568576025267103532034eebed.jpg" alt="Image1">
-                <h3>Le titre de l'album</h3>
+                <a href="#"><h3>Le titre de l'album</h3></a>
             </div>
 
             <div class="select2 select">
                 <img src="https://i.pinimg.com/564x/5e/c2/8f/5ec28fcf22c7f1142b0b6d723f2da610.jpg" alt="Image1">
-                <h3>Le titre de l'album</h3>
+                <a href="#"><h3>Le titre de l'album</h3></a>
             </div>
 
             <div class="select3 select">
                 <img src="https://i.pinimg.com/564x/f0/b5/d0/f0b5d07db8608c0fae8fa149b5c323db.jpg" alt="Image1">
-                <h3>Le titre de l'album</h3>
+                <a href="#"><h3>Le titre de l'album</h3></a>
             </div>
         </div>
         
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="https://use.typekit.net/ktf0wmd.css">
     <link rel="stylesheet" href="https://use.typekit.net/ktf0wmd.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200&display=swap" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
