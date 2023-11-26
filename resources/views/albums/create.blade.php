13024dc3daf2dc31bd5bb6d887fb72e704060c9e
Saejin <saejinmx@gmail.com>
2023-11-26 17:53:50 +0100

    Corrections chemins

--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     
-    <form action="{{route(nvAlbum)}}" method="POST">
+    <form action="album.create" method="POST">
         @csrf
         <label for="titre">Titre de l'album :</label>
         <input type="text" name="titre" id="titre">
