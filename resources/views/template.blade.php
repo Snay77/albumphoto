8f3178d6874613c872777ce0e69d232010740e3d
Saejin <saejinmx@gmail.com>
2023-11-26 22:50:06 +0100

    Modification visuel header

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -12,36 +12,34 @@
 </head>
 <body>
     <header>
-        <div class="container">
-            <div>
-                <a href="{{route('albums.index')}}">Tous les albums</a>
-                @auth
-                    <a href="{{route("albums.create")}}">Créer un album </a>
-                    {{-- @if ()--}} {{--on est sur la page d'un album--}}
-                    {{-- <a href="{{route("nvPhoto")}}">Ajouter une photo</a> seulement si on est sur la page d'un album --}}
-                    {{-- @endif  --}}
-                @else
-                
-                @endauth
-            </div>
-    
-            <div>
-                <img src="{{asset('img/logo.png')}}" alt="Logo ToutouPics">
-                <h1 class="logo">ToutouPics</h1>
-            </div>
-    
-            <div>
-                @auth
-                    <a href="{{route("logout")}}"
-                    onclick="document.getElementById('logout').submit(); return false;">Logout</a>
-                    <form id="logout" action="{{route("logout")}}" method="post">
-                        @csrf
-                    </form>
-                @else
-                    <a href="{{route("login")}}">Login</a>
-                    <a href="{{route("register")}}">Register</a>
-                @endauth
-            </div>
+        <div>
+            <a href="{{route('albums.index')}}">Tous les albums</a>
+            @auth
+                <a href="{{route("albums.create")}}">Créer un album </a>
+                {{-- @if ()--}} {{--on est sur la page d'un album--}}
+                {{-- <a href="{{route("nvPhoto")}}">Ajouter une photo</a> seulement si on est sur la page d'un album --}}
+                {{-- @endif  --}}
+            @else
+            rien
+            @endauth
+        </div>
+
+        <div>
+            <img src="{{asset('img/logo.png')}}" alt="Logo ToutouPics">
+            <h1 class="logo">ToutouPics</h1>
+        </div>
+
+        <div>
+            @auth
+                <a href="{{route("logout")}}"
+                onclick="document.getElementById('logout').submit(); return false;">Logout</a>
+                <form id="logout" action="{{route("logout")}}" method="post">
+                    @csrf
+                </form>
+            @else
+                <a href="{{route("login")}}">Login</a>
+                <a href="{{route("register")}}">Register</a>
+            @endauth
         </div>
     </header>
 
