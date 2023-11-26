b90eeaccb1d4d9ec00bf9d5482a8ec0c95d00e82
Snay77 <77ethan.barlet@gmail.com>
2023-11-26 20:55:50 +0100

    ajout de la fonctionnalité de cliquer pour aggrandir une image

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -4,7 +4,26 @@
     <h1>Les photos de {{$album -> titre}} :</h1>
 
     @forelse ($album->photos as $p)
-        <img src="{{$p -> url}}" alt="La photo">
+        <img src="{{$p -> url}}" alt="l'image" id="imgorigine">
+        <div id="imgzoom">
+            <span onclick="fermerimg()" style="cursor: pointer; position: absolute; top: 20px; right: 20px; color: white; font-size: 40px;">X</span>
+            <img src="" alt="image zoom" id="imgzoomsrc">
+        </div>
+
+        <script>
+            const imgorigine = document.getElementById('imgorigine');
+            const imgzoom = document.getElementById('imgzoom');
+            const imgzoomsrc = document.getElementById('imgzoomsrc');
+
+            imgorigine.addEventListener('click', function(){
+                imgzoomsrc.src = this.src
+                imgzoom.style.display = 'block'
+            })
+
+            function fermerimg() {
+                imgzoom.style.display = 'none'
+            }
+        </script>
     @empty
         Je ne trouve pas de photos dans cet album
     @endforelse
