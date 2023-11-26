81e2cb1f462cd62c2043d5aeb5cfbf1bf8f55e85
Saejin <saejinmx@gmail.com>
2023-11-26 23:08:51 +0100

    Fin du header

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -13,9 +13,9 @@
 <body>
     <header>
         <div>
-            <a href="{{route('albums.index')}}">Tous les albums</a>
+            <a href="{{route('albums.index')}}" class="mod">Tous les albums</a>
             @auth
-                <a href="{{route("albums.create")}}">Créer un album </a>
+                <a href="{{route("albums.create")}}" class="mod">Créer un album </a>
                 {{-- @if ()--}} {{--on est sur la page d'un album--}}
                 {{-- <a href="{{route("nvPhoto")}}">Ajouter une photo</a> seulement si on est sur la page d'un album --}}
                 {{-- @endif  --}}
@@ -31,14 +31,14 @@
 
         <div>
             @auth
-                <a href="{{route("logout")}}"
+                <a href="{{route("logout")}}" class="mod"
                 onclick="document.getElementById('logout').submit(); return false;">Logout</a>
                 <form id="logout" action="{{route("logout")}}" method="post">
                     @csrf
                 </form>
             @else
-                <a href="{{route("login")}}">Login</a>
-                <a href="{{route("register")}}">Register</a>
+                <a href="{{route("login")}}" class="mod">Login</a>
+                <a href="{{route("register")}}" class="mod">Register</a>
             @endauth
         </div>
     </header>
