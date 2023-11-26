6be1fb5338eabeeabad31fa7a724d29ba6bc3407
Saejin <saejinmx@gmail.com>
2023-11-26 21:49:34 +0100

    Personalisation de la page d'accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -6,4 +6,37 @@
         <h1>Bonjour {{Auth::user()->name}}</h1>
     @endauth
 
+    <section id="bandeau1">
+        <div>
+            <h1>Des albums photos pour les plus beaux toutous !</h1>
+            <button>Voir tous les albums</button>
+        </div>
+        <img src="{{asset('img/accueil.jpg')}}" alt="dogo">
+    </section>
+
+    <section id="bandeau2">
+        <b>Admirez et partagez les plus belles photos de vos petits compagnons.</b>
+    </section>
+
+    <section id="bandeau3">
+        <h2>La séléction du mois</h2>
+        <div id="selection">
+            <div class="select1 select">
+                <img src="https://i.pinimg.com/564x/19/83/15/198315568576025267103532034eebed.jpg" alt="Image1">
+                <h3>Le titre de l'album</h3>
+            </div>
+
+            <div class="select2 select">
+                <img src="https://i.pinimg.com/564x/5e/c2/8f/5ec28fcf22c7f1142b0b6d723f2da610.jpg" alt="Image1">
+                <h3>Le titre de l'album</h3>
+            </div>
+
+            <div class="select3 select">
+                <img src="https://i.pinimg.com/564x/f0/b5/d0/f0b5d07db8608c0fae8fa149b5c323db.jpg" alt="Image1">
+                <h3>Le titre de l'album</h3>
+            </div>
+        </div>
+        
+    </section>
+
 @endsection
