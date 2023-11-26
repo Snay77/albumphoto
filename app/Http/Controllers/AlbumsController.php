4ee0dea7d207f2e2068eabec3353116720627c27
Snay77 <77ethan.barlet@gmail.com>
2023-11-26 17:35:52 +0100

    test

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Album;
 use App\Models\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AlbumsController extends Controller
 {
@@ -39,11 +40,11 @@ class AlbumsController extends Controller
         $album = new Album();
         $album->titre = $request->input("titre");
         $album->photo = $request->input("photos");
+        $album->creation = date('Y-m-d');
+        $album->user_id = Auth::id();
         $album->save();
         $album->acteurs()->attach($request->input("albums")); // ça éxécute ce qu'il y a entre les parenthèse
         return redirect()->route("album",$album->id);
-
-        $image = "https://img.nrj.fr/kVRpY7AYUnngF1TATvbvF8VBa0k=/medias%2F2022%2F01%2F61e002bd4184d_61e002c4260f8.jpeg";
 
         foreach($request->file('photos') as $file){
             $f = $file->hashName();         // Je récupère un hash de son nom
