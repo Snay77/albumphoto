a87b6026fef15a5ec04d7db98db5c00f01e5e1a6
Saejin <saejinmx@gmail.com>
2023-11-30 19:51:23 +0100

    Fin du formaulaire de connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,23 +18,28 @@
     <p>Vous connecter à votre compte</p>
     
     @csrf
+
+    <div class="inp">
+      <span class="input-item">
+        <i class='bx bx-user-circle'></i>
+      </span>
+      <input type="email" class="form-input" name="email" required placeholder="Adresse mail"/><br/>
+    </div>
+
+    <div class="inp">
+      <span class="input-item">
+        <i class='bx bxs-key' ></i>
+      </span>
+      <input type="password" class="form-input" name="password" required placeholder="Mot de passe"/>
+      <span>
+        <i class='bx bx-low-vision' aria-hidden="true"  type="button" id="eye"></i>
+      </span><br/>
+    </div>
     
-    <span class="input-item">
-      <i class='bx bx-user-circle'></i>
-    </span>
-    <input type="email" class="form-input" id="txt-input" name="email" required placeholder="Adresse mail"/><br/>
-    
-    <span class="input-item">
-      <i class='bx bxs-key' ></i>
-     </span>
-    <input type="password" class="form-input" name="password" required placeholder="Mot de passe"/>
-    <span>
-      <i class='bx bx-low-vision' aria-hidden="true"  type="button" id="eye"></i>
-   </span><br/>
-    
-    Se souvenir de moi<input type="checkbox" name="remember"/><br/>
-    
-    <input type="submit" class="log-in"/><br/>
+    <div class="bas">
+      <span><input type="checkbox" name="remember"/>Se souvenir de moi<br/></span>
+      <input type="submit" class="log-in"/><br/>
+    </div>
 
   </form>
 
