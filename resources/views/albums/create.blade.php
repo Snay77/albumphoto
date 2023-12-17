218c3a987de757f30a6193cd4ec6124abbafa68d
Saejin <saejinmx@gmail.com>
2023-12-17 22:59:50 +0100

    FIN CSS HOURA !

--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -1,25 +1,26 @@
 @extends('template')
 
 @section('content')
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-    <form action="/albums" method="POST" enctype="multipart/form-data">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <form action="/albums" method="POST" enctype="multipart/form-data" class="creerAlb">
         @csrf
+        <h1>Ajouter un album</h1>
         <label for="titre">Titre de l'album :</label>
         <input type="text" name="titre" id="titre">
         <div id="photos-container">
             <div class="createalbumphoto">
-                <input type="text" name="titrephoto[]" placeholder="Titre de la photo...">
-                <label for="photo">Séléctionne une photo</label>
+                <input type="text" name="titrephoto[]" placeholder="Titre de la photo">
                 <input type="file" name="photos[]" multiple id="photo">
-                <input type="text" name="tag[]" placeholder="Tag de la photo...">
+                <input type="text" name="tag[]" placeholder="Tag de la photo">
                 <input type="number" name="note[]" placeholder="Note de la photo (max : 5)">
                 <button class="supprimerphoto">Supprimer</button>
             </div>
