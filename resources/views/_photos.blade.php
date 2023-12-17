891d09b87d428ff76e50e52a07ce5b9467580628
Lou-Anne <saejinmx@gmail.com>
2023-12-17 12:26:15 +0100

    Début des ennuis ?

--- resources/views/_photos.blade.php
+++ resources/views/_photos.blade.php
@@ -1,9 +1,21 @@
-@forelse ($photos as $p)
-<img src="{{$p -> url}}" alt="l'image" id="imgorigine">
-<div id="imgzoom">
-    <span class="closed" style="cursor: pointer; position: absolute; top: 20px; right: 20px; color: white; font-size: 40px;">X</span>
-    <img src="" alt="image zoom" id="imgzoomsrc">
+<div id="phoAlb">
+
+
+    @forelse ($photos as $p)
+    <div id="phoAlb">
+        <div class="img">
+            <img src="{{$p -> url}}" alt="l_image" id="imgorigine">
+            <i class='bx bxs-x-circle'></i>
+            <i class='bx bx-expand'></i>
+        </div>
+    </div>
+
+    <div id="imgzoom">
+        <span class="closed" style="cursor: pointer; position: absolute; top: 20px; right: 20px; color: white; font-size: 40px;">X</span>
+        <img src="" alt="image zoom" id="imgzoomsrc">
+    </div>
+
+    @empty
+    Je ne trouve pas de photos 
+    @endforelse
 </div>
-@empty
-Je ne trouve pas de photos 
-@endforelse
