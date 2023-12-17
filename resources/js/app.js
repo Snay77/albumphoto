583a69f6d05c9ff54fa40fc2a22480a5cfd7f4fe
Snay77 <77ethan.barlet@gmail.com>
2023-12-17 11:27:02 +0100

    ajout multiple ajout de photo avec js

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,4 +27,33 @@ import './bootstrap';
 
 // Rajouter des lignes de photo lors de l'ajout de l'album
 
-    const ajouterphoto = document.querySelector(".ajouter-photo")
+document.addEventListener('DOMContentLoaded', function() {
+    const ajouterPhotoBouton = document.querySelector('.ajouterphoto');
+    const photosContainer = document.getElementById('photos-container');
+    const photoModele = document.querySelector('.createalbumphoto');
+
+    ajouterPhotoBouton.addEventListener('click', function(e) {
+        e.preventDefault();
+        const ChampPhoto = photoModele.cloneNode(true);
+        photosContainer.appendChild(ChampPhoto);
+        const supprimerChampBouton = document.querySelectorAll('.supprimerphoto');
+        deletePhoto(supprimerChampBouton);
+    });
+
+    function deletePhoto(supprimerChampBouton) {
+        supprimerChampBouton.forEach(function(button) {
+            button.addEventListener('click', function(e) {
+                e.preventDefault();
+                const ChampPhoto = this.parentElement;
+                photosContainer.removeChild(ChampPhoto);
+            });
+        });
+    }
+
+    const supprimerChampBouton = document.querySelectorAll('.supprimerphoto');
+    deletePhoto(supprimerChampBouton);
+
+});
+
+
+
--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -14,12 +14,15 @@
         @csrf
         <label for="titre">Titre de l'album :</label>
         <input type="text" name="titre" id="titre">
-        <div class="createalbumphoto">
-            <input type="text" name="titrephoto[]" placeholder="Titre de la photo...">
-            <label for="photo">Séléctionne une photo</label>
-            <input type="file" name="photos[]" multiple id="photo">
-            <input type="text" name="tag[]" placeholder="Tag de la photo...">
-            <input type="number" name="note[]" placeholder="Note de la photo (max : 5)">
+        <div id="photos-container">
+            <div class="createalbumphoto">
+                <input type="text" name="titrephoto[]" placeholder="Titre de la photo...">
+                <label for="photo">Séléctionne une photo</label>
+                <input type="file" name="photos[]" multiple id="photo">
+                <input type="text" name="tag[]" placeholder="Tag de la photo...">
+                <input type="number" name="note[]" placeholder="Note de la photo (max : 5)">
+                <button class="supprimerphoto">Supprimer</button>
+            </div>
         </div>
         <button class="ajouterphoto">Ajouter une photo</button>
         <input type="submit"><br/>
