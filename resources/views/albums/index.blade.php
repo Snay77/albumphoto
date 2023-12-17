7b26ae823b627cf1867e89c56a86da220269459b
Snay77 <77ethan.barlet@gmail.com>
2023-12-17 13:07:16 +0100

    ajout des bonnes photos pour chaque album

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -18,7 +18,7 @@
         @foreach ($albums as $a)
             <div class="unAlbum">
                 <a href="{{route("albums.show", $a -> id)}}">
-                    <img src="https://i.pinimg.com/564x/c5/d5/56/c5d5568ffdb855aca2e790ee883da413.jpg" alt="l'image de l'album">
+                    <img src="{{ $a->photos->first()->url ?? 'https://i.pinimg.com/564x/c5/d5/56/c5d5568ffdb855aca2e790ee883da413.jpg'}}" alt="l'image de l'album">
                     <p>{{$a -> creation}}</p>
                     <p class="titreAlb">{{$a -> titre}}</p>
                     <p class="lienAlb">Voir l'album</p>
